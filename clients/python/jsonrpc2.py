d82afd7268f1410372d81a9a439c8293f594cf14
Dmitry Antonyuk <dantonyuk@hylamobile.com>
2019-10-22 18:59:39 -0500

    Add python client

---
+++ clients/python/jsonrpc2.py
@@ -0,0 +1,171 @@
+import requests
+import json
+
+
+class JsonRpcError(Exception):
+    def __init__(self, message, error):
+        super().__init__(message)
+        self.error = error
+
+
+class HttpError(Exception):
+    def __init__(self, message, status):
+        super().__init__(message)
+        self.status = status
+
+
+class JsonRpcClient:
+    def __init__(self, url, *args, **kwargs):
+        self._url = url
+        self._req_args = args
+        self._req_kwargs = kwargs
+
+    def _post(self, payload):
+        if (self._req_kwargs.get("headers")):
+            self._req_kwargs["headers"]["Content-Type"] = "application/json;charset=utf-8"
+        response = requests.post(self._url, data=json.dumps(payload), *self._req_args, **self._req_kwargs)
+        response.raise_for_status()
+        if response.status_code == 200:
+            resp_json = response.json()
+            error = resp_json.get("error")
+            if error:
+                raise JsonRpcError(error)
+            return DictAdapter.create(resp_json.get("result"))
+        else:
+            return None
+
+    def _curl(self, payload):
+        headers = self._req_kwargs.get("headers", {})
+        headers["Content-Type"] = "application/json;charset=utf-8"
+        headers = ' \\\n'.join(["-H '%s:%s'" % (k,v) for k, v in headers.items()])
+        return "curl -X POST \\\n%s \\\n-d '%s' \\\n'%s'" % (headers, json.dumps(payload), self._url)
+
+    def _httpie(self, payload):
+        return "http '%s' \\\n<<< '%s'" % (self._url, json.dumps(payload))
+
+    def batch(self):
+        return JsonRpcBatch(self)
+
+    def __getattr__(self, attr):
+        return JsonRpcMethod(self, attr)
+
+class JsonRpcMethod:
+    def __init__(self, client, method):
+        self._client = client
+        self._method = method
+
+    def __call__(self, *args, **kwargs):
+        params = args and args or kwargs
+        return self._client._post({
+            "id": 1,
+            "jsonrpc": "2.0",
+            "method": self._method,
+            "params": params
+        })
+
+    def notify(*args, **kwargs):
+        params = args and args or kwargs
+        return self._client._post({
+            "jsonrpc": "2.0",
+            "method": self._method,
+            "params": params
+        })
+
+    def curl(self, *args, **kwargs):
+        params = args and args or kwargs
+        return self._client._curl({
+            "id": 1,
+            "jsonrpc": "2.0",
+            "method": self._method,
+            "params": params
+        })
+
+    def httpie(self, *args, **kwargs):
+        params = args and args or kwargs
+        return self._client._httpie({
+            "id": 1,
+            "jsonrpc": "2.0",
+            "method": self._method,
+            "params": params
+        })
+
+
+class JsonRpcBatch:
+    def __init__(self, client):
+        self._client = client
+        self.batch = []
+        self.seq = 0
+
+    def call(self, method, params):
+        self.seq += 1
+        self.batch.append({
+            "id": self.seq,
+            "jsonrpc": "2.0",
+            "method": method,
+            "params": params
+        })
+        return self
+
+    def notify(self, method, params):
+        self.batch.append({
+            "jsonrpc": "2.0",
+            "method": method,
+            "params": params
+        })
+        return self
+
+    def __getattr__(self, attr):
+        return JsonRpcBatchMethod(self, attr)
+
+    def post(self):
+        return self._client._post(self.batch)
+
+    def curl(self, *args, **kwargs):
+        return self._client._curl(self.batch)
+
+    def httpie(self, *args, **kwargs):
+        return self._client._httpie(self.batch)
+
+
+class JsonRpcBatchMethod:
+    def __init__(self, batch, method):
+        self._batch = batch
+        self._method = method
+
+    def __call__(self, *args, **kwargs):
+        params = args and args or kwargs
+        return self._batch.call(self._method, params)
+
+    def notify(self, *args, **kwargs):
+        params = args and args or kwargs
+        return self._batch.notify(self._method, params)
+
+
+class DictAdapter:
+    def __init__(self, obj):
+        self.obj = obj
+
+    @staticmethod
+    def create(obj):
+        if type(obj) in [dict, list]:
+            return DictAdapter(obj)
+        else:
+            return obj
+
+    def __getitem__(self, attr):
+        return DictAdapter.create(self.obj[attr])
+
+    def __getattr__(self, attr):
+        return DictAdapter.create(self.obj[attr])
+
+    def __iter__(self):
+        return iter(self.obj)
+
+    def __len__(self):
+        return len(self.obj)
+
+    def __str__(self):
+        return str(self.obj)
+
+    def __repr__(self):
+        return repr(self.obj)
