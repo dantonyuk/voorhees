91d936399085e89f548e44e8e7e88827b180b872
Dmitry Antonyuk <dantonyuk@hylamobile.com>
2019-11-13 20:29:23 -0600

    Fix python client

--- clients/python/jsonrpc2.py
+++ clients/python/jsonrpc2.py
@@ -23,13 +23,13 @@ class JsonRpcClient:
     def _post(self, payload):
         if (self._req_kwargs.get("headers")):
             self._req_kwargs["headers"]["Content-Type"] = "application/json;charset=utf-8"
-        response = requests.post(self._url, data=json.dumps(payload), *self._req_args, **self._req_kwargs)
+        response = requests.post(self._url, data=_json_dumps(payload), *self._req_args, **self._req_kwargs)
         response.raise_for_status()
         if response.status_code == 200:
             resp_json = response.json()
             error = resp_json.get("error")
             if error:
-                raise JsonRpcError(error)
+                raise JsonRpcError(error.get("message"), error)
             return DictAdapter.create(resp_json.get("result"))
         else:
             return None
@@ -38,10 +38,10 @@ class JsonRpcClient:
         headers = self._req_kwargs.get("headers", {})
         headers["Content-Type"] = "application/json;charset=utf-8"
         headers = ' \\\n'.join(["-H '%s:%s'" % (k,v) for k, v in headers.items()])
-        return "curl -X POST \\\n%s \\\n-d '%s' \\\n'%s'" % (headers, json.dumps(payload), self._url)
+        return "curl -X POST \\\n%s \\\n-d '%s' \\\n'%s'" % (headers, _json_dumps(payload), self._url)
 
     def _httpie(self, payload):
-        return "http '%s' \\\n<<< '%s'" % (self._url, json.dumps(payload))
+        return "http '%s' \\\n<<< '%s'" % (self._url, _json_dumps(payload))
 
     def batch(self):
         return JsonRpcBatch(self)
@@ -55,16 +55,16 @@ class JsonRpcMethod:
         self._method = method
 
     def __call__(self, *args, **kwargs):
-        return self._client._post(__json_request(self._method, __params(*args, **kwargs), id=1)
+        return self._client._post(_json_request(self._method, _param(*args, **kwargs), id=1))
 
-    def notify(*args, **kwargs):
-        return self._client._post(__json_request(self._method, __params(*args, **kwargs)))
+    def notify(self, *args, **kwargs):
+        return self._client._post(_json_request(self._method, _param(*args, **kwargs)))
 
     def curl(self, *args, **kwargs):
-        return self._client._curl(__json_request(self._method, __params(*args, **kwargs), id=1))
+        return self._client._curl(_json_request(self._method, _param(*args, **kwargs), id=1))
 
     def httpie(self, *args, **kwargs):
-        return self._client._httpie(__json_request(self._method, __params(*args, **kwargs), id=1))
+        return self._client._httpie(_json_request(self._method, _param(*args, **kwargs), id=1))
 
 
 class JsonRpcBatch:
@@ -75,11 +75,11 @@ class JsonRpcBatch:
 
     def call(self, method, params):
         self.seq += 1
-        self.batch.append(__json_request(methods, params, self.seq))
+        self.batch.append(_json_request(methods, params, self.seq))
         return self
 
     def notify(self, method, params):
-        self.batch.append(__json_request(methods, params))
+        self.batch.append(_json_request(methods, params))
         return self
 
     def __getattr__(self, attr):
@@ -101,17 +101,17 @@ class JsonRpcBatchMethod:
         self._method = method
 
     def __call__(self, *args, **kwargs):
-        return self._batch.call(self._method, __params(*args, **kwargs))
+        return self._batch.call(self._method, _param(*args, **kwargs))
 
     def notify(self, *args, **kwargs):
-        return self._batch.notify(self._method, __params(*args, **kwargs))
+        return self._batch.notify(self._method, _param(*args, **kwargs))
 
 
-def __params(*args, **kwargs):
+def _param(*args, **kwargs):
     return args and args or kwargs
 
 
-def __json_request(method, params=None, id=None):
+def _json_request(method, params=None, id=None):
     return {
         "id": id,
         "jsonrpc": "2.0",
@@ -119,6 +119,10 @@ def __json_request(method, params=None, id=None):
         "params": params
     }
 
+def _json_dumps(obj):
+    if type(obj) == DictAdapter:
+        return _json_dumps(obj.obj)
+    return json.dumps(obj)
 
 class DictAdapter:
     def __init__(self, obj):
